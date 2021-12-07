14e5c8fd89b9626225c4916223aa8cc96a92c8aa
AlanSauqui <0227221@up.edu.mx>
2021-12-06 18:27:23 -0600

    Se añadieron consultas y se modificaron otras para que salieran todos los datos y no sólo el top 200.

--- controllers/sports.js
+++ controllers/sports.js
@@ -20,7 +20,7 @@ const getTeams = async (req, res) => {
         }
 
 
-        var querystring = `select top 200 e.IDEquipo, NombreEquipo, NombreEstadio, CapacidadEstadio, UbicacionEstadio, StrImagenEquipo, StrImagenEstadio, AnnoFundacion, Descripcion, p.NombrePais Pais from Equipo e
+        var querystring = `select e.IDEquipo, NombreEquipo, NombreEstadio, CapacidadEstadio, UbicacionEstadio, StrImagenEquipo, StrImagenEstadio, AnnoFundacion, Descripcion, p.NombrePais Pais from Equipo e
                             join Pais p on e.IDPais=p.IDPais`;
 
         var sqlrequest = new sql.Request();
@@ -86,7 +86,7 @@ const getTeam = async (req, res) => {
         }
 
 
-        var querystring = `select top 200 e.IDEquipo, NombreEquipo, NombreEstadio, CapacidadEstadio, UbicacionEstadio, StrImagenEquipo, StrImagenEstadio, AnnoFundacion, Descripcion, p.NombrePais Pais from Equipo e
+        var querystring = `select e.IDEquipo, NombreEquipo, NombreAlternoEquipo, NombreEstadio, CapacidadEstadio, UbicacionEstadio, StrImagenEquipo, StrImagenEstadio, StrBanner, StrJersey, genero, AnnoFundacion, Descripcion, p.NombrePais Pais from Equipo e
                             join Pais p on e.IDPais=p.IDPais
                             where e.IDEquipo=${idTeam}`;
 
@@ -109,8 +109,9 @@ const getLeagues = async (req, res) => {
         }
 
 
-        var querystring = `select top 200 IDLiga, NombreLiga, NombreAlterno, d.NombreDeporte Deporte from Liga l
-                            join Deporte d on d.IDDeporte=l.IDDeporte`;
+        var querystring = `select IDLiga, NombreLiga, NombreAlterno, d.NombreDeporte Deporte, p.NombrePais Pais from Liga l
+                            join Deporte d on d.IDDeporte=l.IDDeporte
+                            join Pais p on p.IDPais=l.IDPais`;
 
         var sqlrequest = new sql.Request();
 
@@ -187,8 +188,9 @@ const getSportLeagues = async (req, res) => {
         }
 
 
-        var querystring = `select l.IDLiga, NombreLiga, NombreAlterno, d.NombreDeporte Deporte from Deporte d
+        var querystring = `select l.IDLiga, NombreLiga, NombreAlterno, d.NombreDeporte Deporte, p.NombrePais Pais from Deporte d
                             join Liga l on l.IDDeporte=d.IDDeporte
+                            join Pais p on p.IDPais=l.IDPais
                             where d.IDDeporte=${idSport}`;
 
         var sqlrequest = new sql.Request();
@@ -212,11 +214,70 @@ const getSearchTeam = async (req, res) => {
         }
 
 
-        var querystring = `select e.IDEquipo, NombreEquipo, NombreEstadio, CapacidadEstadio, UbicacionEstadio, StrImagenEquipo, StrImagenEstadio, AnnoFundacion, Descripcion, p.NombrePais Pais, l.NombreLiga Liga from Equipo e
+        // var querystring = `select e.IDEquipo, NombreEquipo, NombreEstadio, CapacidadEstadio, UbicacionEstadio, StrImagenEquipo, StrImagenEstadio, AnnoFundacion, Descripcion, p.NombrePais Pais, l.NombreLiga Liga from Equipo e
+        //                     join Pais p on e.IDPais=p.IDPais
+        //                     join EquipoLiga el on el.IDEquipo=e.IDEquipo
+        //                     join Liga l on l.IDLiga=el.IDLiga
+        //                     where e.NombreEquipo like '%${teamName}%'`;
+
+        var querystring = `select e.IDEquipo, NombreEquipo, NombreEstadio, CapacidadEstadio, UbicacionEstadio, StrImagenEquipo, StrImagenEstadio, AnnoFundacion, Descripcion, p.NombrePais Pais 
+                            from Equipo e
+                            join Pais p on e.IDPais=p.IDPais
+                            where e.NombreEquipo like '%${teamName}%'
+                            order by NombreEquipo`;
+
+        var sqlrequest = new sql.Request();
+
+        sqlrequest.query(querystring, (err, recordset) => {
+            if(err){
+                console.log(err);
+            }
+            res.send(recordset);
+        });
+
+    });
+}
+
+const getCountSportLeague = async (req, res) => {
+    sql.connect(configsql, (err) => {
+        if(err){
+            console.log(err);
+        }
+
+
+        var querystring =   `select d.NombreDeporte NombreDeporte, count(1) Cantidad
+                            from Deporte d
+                            join Liga l on l.IDDeporte=d.IDDeporte
+                            group by d.NombreDeporte
+                            order by count(1)`;
+
+        var sqlrequest = new sql.Request();
+
+        sqlrequest.query(querystring, (err, recordset) => {
+            if(err){
+                console.log(err);
+            }
+            res.send(recordset);
+        });
+
+    });
+}
+
+const getLeagueByTeamInSpecific = async (req, res) => {
+    const idTeam = req.params.idTeam;
+
+    sql.connect(configsql, (err) => {
+        if(err){
+            console.log(err);
+        }
+
+        var querystring = `select l.IDLiga, l.NombreLiga
+                            from Equipo e
                             join Pais p on e.IDPais=p.IDPais
                             join EquipoLiga el on el.IDEquipo=e.IDEquipo
                             join Liga l on l.IDLiga=el.IDLiga
-                            where e.NombreEquipo like '%${teamName}%'`;
+                            where e.IDEquipo=${idTeam}
+                            order by l.NombreLiga`
 
         var sqlrequest = new sql.Request();
 
@@ -239,5 +300,7 @@ module.exports = {
     getSports,
     getLeagueTeams,
     getCountries,
-    getSearchTeam
+    getSearchTeam,
+    getCountSportLeague,
+    getLeagueByTeamInSpecific
 }
